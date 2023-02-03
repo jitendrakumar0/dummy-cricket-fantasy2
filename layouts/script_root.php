d5b24caaa7a51d8369ec5976fe40ee20bdffdc8a
jitendrakumar0 <70063345+jitendrakumar0@users.noreply.github.com>
2023-02-03 10:48:31 +0530

    Update script_root.php

--- layouts/script_root.php
+++ layouts/script_root.php
@@ -3,9 +3,9 @@ $get_url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $arr = explode("?", $get_url, 2);
 $actual_link = $arr[0];
 if ($_SERVER["HTTP_HOST"] == "localhost") {
-   define('f_path', 'http://localhost/frist-fantasy/');
+   define('f_path', 'http://localhost/dummy-cricket-fantasy2/');
 } else if ($_SERVER['HTTP_HOST'] == '192.168.1.50') {
-   define('f_path', 'http://192.168.1.50/jitendra/frist-fantasy/');
+   define('f_path', 'http://192.168.1.50/jitendra/dummy-cricket-fantasy2/');
 } else {
-   define('f_path', 'http://143.110.244.110/jitendra/frist-fantasy/');
+   define('f_path', 'http://143.110.244.110/jitendra/dummy-cricket-fantasy2/');
 }
